56688998620bea321f015746e9cc82b96689e1dc
Luca Pagano <luca.pagano@neodatagroup.com>
2021-09-04 11:26:56 +0200

    Push first aws configurations

---
+++ aws/variables.tf
@@ -0,0 +1,9 @@
+variable "profile" {
+    default     = "default"
+    description = "AWS Credentials profile"
+}
+  
+variable "region" {
+    default     = "eu-west-1"
+    description = "AWS region"
+}
---
+++ aws/variables.tfvars
@@ -0,0 +1,4 @@
+# AWS Profile to use for create the infrastructure
+profile              = "default"
+# AWS Region to use
+region               = "eu-west-1"
